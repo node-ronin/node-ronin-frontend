c5be96f480a01f486822c116574ba9a4d317769f
alteregogi <ginanjar.lillunatic@gmail.com>
2023-01-02 21:29:54 +0700

    using daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '940px',
+        xl: '1024px',
+        '2xl': '940px'
+      }
+    }
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    // require("flowbite/plugin"),
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  }
 };
