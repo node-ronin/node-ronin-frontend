ecea263aef34c93926c4352c5d62c77d32300170
alteregogi <ginanjar.lillunatic@gmail.com>
2023-01-20 13:55:00 +0700

    update navigation
    - create config for menus
    - add mobile menu

---
+++ src/lib/config.ts
@@ -0,0 +1,18 @@
+export const list_menu = [
+    {
+        title: "Home",
+        link: "/"
+    },
+    {
+        title: "Blog",
+        link: "/blog"
+    },
+    {
+        title: "About",
+        link: "/#"
+    },
+    {
+        title: "Services",
+        link: "/#"
+    }
+]
